31c5f0d1c92ca01337d037befcbd654081386ec6
Pritam Saha <saha.pritam.work@gmail.com>
2023-07-05 11:31:59 +0530

    Topic-5

--- springjdbc/src/main/java/org/pritam/springjdbc/DAO/CustomerDAOImplementation.java
+++ springjdbc/src/main/java/org/pritam/springjdbc/DAO/CustomerDAOImplementation.java
@@ -1,4 +1,11 @@
 package org.pritam.springjdbc.DAO;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class CustomerDAOImplementation implements CustomerDAO {
+	private JdbcTemplate jdbcTemplate;
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 }
